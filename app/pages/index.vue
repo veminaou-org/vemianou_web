a341567cded42f4e8be59bfd8eef9539a1a4def9
angelica <atogbevi.wk@gmail.com>
2025-11-04 18:23:23 +0000

    feat: add hero section with infinite auto-sliding carousel and add the commponent on landing page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,7 @@
 <template>
   <div>
     <!-- Section Hero -->
-    <section class="min-h-screen bg-gray-background flex items-center justify-center">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-        <h1 class="text-5xl font-bold text-primary mb-6">
-          Bienvenue sur le site de l'Association VEMIANOU
-        </h1>
-        <p class="text-xl text-gray-text">
-          Accueil Hero
-        </p>
-      </div>
-    </section>
+    <HeroSection />
 
     <!-- Section A propos -->
     <section id="a-propos" class="min-h-screen bg-white flex items-center justify-center">
